f43752acfbd2891ff22ec21e3269ea7a5cc35dae
Steigs <mindfulhitting@proton.me>
2025-03-17 21:40:49 -0700

    another one

---
+++ Daily Python/ch10_exceptions.py
@@ -0,0 +1,15 @@
+# page 197 with Anthem by Ayn Rand
+
+from pathlib import Path
+
+path = Path('anthem.txt')
+
+try: 
+    contents = path.read_text()
+except FileNotFoundError:
+    print(f"Sorry that file {path} does not exist!")
+else: 
+    # Count the approximate number of words in the file:
+    words =contents.split()
+    num_words=len(words)
+    print(f"The file {path} has abotu {num_words} words.")
