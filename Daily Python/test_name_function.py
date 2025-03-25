517391641a31acddd9dacab40324cfc3a135c4be
Steigs <mindfulhitting@proton.me>
2025-03-25 15:38:00 -0700

    figured out pytest

--- Daily Python/test_name_function.py
+++ Daily Python/test_name_function.py
@@ -7,4 +7,7 @@ def test_first_last_name():
     formatted_name = get_formatted_name ('janis', 'joplin')
     assert formatted_name =="Janis Joplin"
 
-    
+def test_first_last_middle_names():
+    '''Do names like Brady Michael Steiger work?'''
+    formatted_name = get_formatted_name('brady', 'steiger' ,'michael')
+    assert formatted_name == "Brady Michael Steiger"
