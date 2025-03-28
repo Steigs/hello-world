410a9de1976eef65c3f6c7f65f684374b4f33838
Steigs <mindfulhitting@proton.me>
2025-03-28 14:53:24 -0700

    daily

--- Daily Python/test_survey.py
+++ Daily Python/test_survey.py
@@ -1,10 +1,25 @@
 # page 220
-
+import pytest
 from ch11_survey import AnonymousSurvey
+
+pytest.fixture
+def language_survey():
+    '''A survey that will be available to all test functions'''
+    question = "What language did you first learn to speak?"
+    language_survey = AnonymousSurvey(question)
+    return language_survey
+
 
 def test_store_single_response():
     '''Test that a single response is stored properly'''
-    question = "What language did you first learn to speak"
-    language_survey = AnonymousSurvey(question)
     language_survey.store_response('English')
     assert 'English' in language_survey.responses
+
+def test_store_three_responses():
+    '''Test that three individual responses are stores properly.'''
+    responses = ['English', 'Spanish', 'Mandarin']
+    for response in responses:
+        language_survey.store_response(response)
+
+    for response in responses:
+        assert response in language_survey.responses
