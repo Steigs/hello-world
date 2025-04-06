5d0173c88103d65d8b32f1945d80b2a7a84c829b
Steigs <mindfulhitting@proton.me>
2025-04-05 22:10:26 -0700

    random

--- Daily Python/Data Visualization Project/rw_visual.py
+++ Daily Python/Data Visualization Project/rw_visual.py
@@ -4,14 +4,22 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
+#Keep makaing new walks, as long as the program is active. 
+while True:
 #Make random walk
+    rw= RandomWalk()
+    rw.fill_walk()
 
-rw= RandomWalk()
-rw.fill_walk()
 
-#plot the points in the walk
-plt.style.use('classic')
-fig,ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values , s=15)
-ax.set_aspect('equal')
-plt.show()
+
+    #plot the points in the walk
+    plt.style.use('classic')
+    fig,ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values , s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+
+    keep_running = input ("Make another walk (y/n): ")
+    if keep_running == 'n':
+        break
