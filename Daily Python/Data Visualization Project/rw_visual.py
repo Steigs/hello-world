6ba1244527c70992cd94bd9d7d47dbde7aac3a87
Steigs <mindfulhitting@proton.me>
2025-04-06 21:43:36 -0700

    lazy

--- Daily Python/Data Visualization Project/rw_visual.py
+++ Daily Python/Data Visualization Project/rw_visual.py
@@ -15,8 +15,15 @@ while True:
     #plot the points in the walk
     plt.style.use('classic')
     fig,ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values , s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values , c = point_numbers, cmap=plt.cm.Blues, 
+               edgecolors="none" , s=15)
     ax.set_aspect('equal')
+
+    # Emphasize the first and last points
+    ax.scatter(0,0,c='green',edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
     plt.show()
 
 
