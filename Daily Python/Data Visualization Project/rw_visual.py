f5fa4a9a2f182fc309272e541b3eb29e0ff638c8
Steigs <mindfulhitting@proton.me>
2025-04-07 12:59:18 -0700

    daily

--- Daily Python/Data Visualization Project/rw_visual.py
+++ Daily Python/Data Visualization Project/rw_visual.py
@@ -7,22 +7,23 @@ from random_walk import RandomWalk
 #Keep makaing new walks, as long as the program is active. 
 while True:
 #Make random walk
-    rw= RandomWalk()
+    rw= RandomWalk(5000)
     rw.fill_walk()
-
-
 
     #plot the points in the walk
     plt.style.use('classic')
     fig,ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values , c = point_numbers, cmap=plt.cm.Blues, 
-               edgecolors="none" , s=15)
+    ax.plot(rw.x_values, rw.y_values , linewidth=1)
     ax.set_aspect('equal')
 
     # Emphasize the first and last points
     ax.scatter(0,0,c='green',edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    #Remove Axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
