f6d1165b1c481365b4fc8878a37a421b19a8cf44
Steigs <mindfulhitting@proton.me>
2025-04-13 21:25:08 -0700

    daily

--- Daily Python/Data Visualization Project/weather_data/sitka_highs.py
+++ Daily Python/Data Visualization Project/weather_data/sitka_highs.py
@@ -1,11 +1,32 @@
 from pathlib import Path
 import csv
 
-path = Path("/home/steigs/Desktop/PlebDevs/Daily Python/Data Visualization Project/weather_data/sitka_weather_2021_simple.csv")
+import matplotlib.pyplot as plt
+
+
+path = Path("/home/steigs/Desktop/PlebDevs/Daily Python/Data Visualization Project/weather_data/sitka_weather_07-2021_simple.csv")
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+#Extract high temperatures
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+print (highs)
+
+#Plot the high Temperatures
+plt.style.use('seaborn-v0_8')
+fig, ax=plt.subplots()
+ax.plot(highs, color="red")
+
+# Format plot.
+ax.set_title("Daily High Temperatures, 2021", fontsize=24)
+ax.set_xlabel('',fontsize = 16)
+ax.set_ylabel("Temperature (F)", fontsize= 16)
+ax.tick_params(labelsize=16)
+
+plt.show()
