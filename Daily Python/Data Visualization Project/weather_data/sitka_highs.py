9ad51324d72f70365eb6d503567fe634e4311c2e
Steigs <mindfulhitting@proton.me>
2025-04-15 22:07:10 -0700

    daily

--- Daily Python/Data Visualization Project/weather_data/sitka_highs.py
+++ Daily Python/Data Visualization Project/weather_data/sitka_highs.py
@@ -5,29 +5,30 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-path = Path("/home/steigs/Desktop/PlebDevs/Daily Python/Data Visualization Project/weather_data/sitka_weather_07-2021_simple.csv")
+path = Path("/home/steigs/Desktop/PlebDevs/Daily Python/Data Visualization Project/weather_data/sitka_weather_2021_simple.csv")
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
-#Extract dates and high temperatures
-dates, highs = [], []
+#Extract dates and high and low temperatures
+dates, highs, lows = [], [],[]
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
-print (highs)
-
-# Plot the high Temperatures
+# Plot the high and low Temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')  # Corrected to plot dates on x-axis
+ax.plot(dates, lows, color = 'blue')
 
 # Format plot.
-ax.set_title("Daily High Temperatures, 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()  # This will format the x-axis dates
 ax.set_ylabel("Temperature (F)", fontsize=16)
