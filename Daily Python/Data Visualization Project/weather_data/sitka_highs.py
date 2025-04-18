7a5fde9cb9e14a1e49f40b8926ed65dc472fd3de
Steigs <mindfulhitting@proton.me>
2025-04-18 10:22:20 -0700

    daily

--- Daily Python/Data Visualization Project/weather_data/sitka_highs.py
+++ Daily Python/Data Visualization Project/weather_data/sitka_highs.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-path = Path("/home/steigs/Desktop/PlebDevs/Daily Python/Data Visualization Project/weather_data/sitka_weather_2021_simple.csv")
+path = Path("/home/steigs/Desktop/PlebDevs/Daily Python/Data Visualization Project/weather_data/death_valley_2021_simple.csv")
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
@@ -15,11 +15,16 @@ header_row = next(reader)
 dates, highs, lows = [], [],[]
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = int(row[4])
-    low = int(row[5])
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
+
+    try: 
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
 # Plot the high and low Temperatures
 plt.style.use('seaborn-v0_8')
@@ -29,7 +34,8 @@ ax.plot(dates, lows, color = 'blue', alpha =0.5)
 ax.fill_between(dates, highs, lows, facecolor = 'blue', alpha =0.1)
 
 # Format plot.
-ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
+title= "Daily High and Low Temperatures, 2021/nDeath Valley, CA"
+ax.set_title(title, fontsize=20)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()  # This will format the x-axis dates
 ax.set_ylabel("Temperature (F)", fontsize=16)
